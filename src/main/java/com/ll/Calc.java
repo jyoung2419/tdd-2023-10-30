c596fd9e95582d68d2eefe7894ee8be991c1087c
Jeong jinyoung <jjyoung2419@gmail.com>
2023-10-30 13:06:28 +0900

    10 - 5

--- src/main/java/com/ll/Calc.java
+++ src/main/java/com/ll/Calc.java
@@ -4,12 +4,19 @@ public class Calc {
     public static int run(String exp) {
         if (exp.isBlank()) return 0;
 
-        if (exp.contains("-")) return 0;
+        if (exp.contains("-")) {
+            String[] expBits = exp.split(" \\- ");
 
-        String[] exps = exp.split(" \\+ ");
+            int num1 = Integer.parseInt(expBits[0]);
+            int num2 = Integer.parseInt(expBits[1]);
 
-        int num1 = Integer.parseInt(exps[0]);
-        int num2 = Integer.parseInt(exps[1]);
+            return num1 - num2;
+        }
+
+        String[] expBits = exp.split(" \\+ ");
+
+        int num1 = Integer.parseInt(expBits[0]);
+        int num2 = Integer.parseInt(expBits[1]);
 
         return num1 + num2;
     }
--- src/test/java/com/ll/CalcTest.java
+++ src/test/java/com/ll/CalcTest.java
@@ -7,14 +7,14 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class CalcTest {
     @Test
-    @DisplayName("Calc에 run 메소드는 int를 리턴한다.")
-    void t1(){
+    @DisplayName("Calc에 run 메서드는 int를 리턴한다.")
+    void t1() {
         int rs = Calc.run("");
     }
 
     @Test
     @DisplayName("10 + 10 = 20")
-    void t2(){
+    void t2() {
         int rs = Calc.run("10 + 10");
 
         assertThat(rs).isEqualTo(20);
@@ -22,7 +22,7 @@ public class CalcTest {
 
     @Test
     @DisplayName("10 + 20 = 30")
-    void t3(){
+    void t3() {
         int rs = Calc.run("10 + 20");
 
         assertThat(rs).isEqualTo(30);
@@ -30,7 +30,7 @@ public class CalcTest {
 
     @Test
     @DisplayName("10 + 30 = 40")
-    void t4(){
+    void t4() {
         int rs = Calc.run("10 + 30");
 
         assertThat(rs).isEqualTo(40);
@@ -38,7 +38,7 @@ public class CalcTest {
 
     @Test
     @DisplayName("10 + 40 = 50")
-    void t5(){
+    void t5() {
         int rs = Calc.run("10 + 40");
 
         assertThat(rs).isEqualTo(50);
@@ -46,9 +46,17 @@ public class CalcTest {
 
     @Test
     @DisplayName("10 - 10 = 0")
-    void t6(){
+    void t6() {
         int rs = Calc.run("10 - 10");
 
         assertThat(rs).isEqualTo(0);
+    }
+
+    @Test
+    @DisplayName("10 - 5 = 5")
+    void t7() {
+        int rs = Calc.run("10 - 5");
+
+        assertThat(rs).isEqualTo(5);
     }
 }
